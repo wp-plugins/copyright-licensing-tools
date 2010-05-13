7668cb2ca8d6a76ea939f06e01711e91267da5e8
icopyright <icopyright@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-13 14:28:31 +0000

    tagging version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/copyright-licensing-tools/tags/1.0.1@240618 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ icopyright.php
@@ -0,0 +1,97 @@
+<?php
+/*
+Plugin Name: iCopyright
+Plugin URI: http://info.icopyright.com/wordpress-plugin
+Description: The iCopyright plugin will add article tools that enable visitors to your site to print, email, post, and republish your posts. This includes ad-supported free uses as well as licensing options for a fee. This plugin also adds an interactive copyright notice at the bottom of your pages. To fully activate the plugin, you must click on "Settings" below to register your site/blog and get a Publication ID. For detailed instructions, click the "Visit Plugin Site" link below.
+Author: iCopyright, Inc.  
+Author URI: http://info.icopyright.com
+Version: 1.0
+*/
+
+
+//define constant that need to be changed from test environment to live environment
+
+//define URL to iCopyright API
+define("ICOPYRIGHT_API_URL","http://license.icopyright.net/api/xml/publisher/add");
+
+//define URL to iCopyright
+//assuming other file structures will be intact.
+define("ICOPYRIGHT_URL","http://license.icopyright.net/");
+
+
+
+//leave this page for including functions organised into files.
+//for easy reference.
+
+//include plugin functions
+include (WP_PLUGIN_DIR . '/icopyright/icopyright-functions.php');
+
+//include plugin admin page
+include (WP_PLUGIN_DIR . '/icopyright/icopyright-admin.php');
+
+
+//hook in icopyright-interactive-tools.css into theme template <head>
+function load_icopyright_script(){
+//register script
+wp_register_script('icopyright-notice-js', WP_PLUGIN_URL . '/icopyright/icopyright-interactive-tools.js','', '1.0');
+wp_register_style('icopyright_notice', WP_PLUGIN_URL.'/icopyright/icopyright-interactive-tools.css', $deps, '1.0', 'screen');
+//load script
+wp_enqueue_script('icopyright-notice-js', WP_PLUGIN_URL . '/icopyright/icopyright-interactive-tools.js','', '1.0');
+wp_enqueue_style( 'icopyright_notice', WP_PLUGIN_URL.'/icopyright/icopyright-interactive-tools.css', $deps, '1.0', 'screen' );
+} 
+
+//add script to theme <head>
+add_action('init', 'load_icopyright_script');
+
+
+//hook for icopyright toolbar float
+function icopyright_toolbar_float(){
+do_action('icopyright_toolbar_float');
+}
+
+//hook in css if blogger selected icopyright article tools alignment in WordPress Admin
+//use this css to add on to class="icopyright-horizontal-interactive-toolbar" and class="icopyright-vertical-interactive-toolbar"
+function load_icopyright_alignment_css(){
+
+$css = get_option('icopyright_admin');
+$toolbar_alignment = $css['align'];
+
+if($toolbar_alignment=="right"){
+
+//use heredox syntax
+$str = <<<CSS
+\n
+<!--icopyright embedded css -->
+<style type="text/css">
+.icx-toolbar{
+float:right;
+margin:0px 0px 10px 10px;
+}
+</style>\n
+CSS;
+echo $str;
+
+}//end if
+
+}
+add_action('icopyright_toolbar_float','load_icopyright_alignment_css');
+
+
+
+//The following filters seems to work only on default plugin page.
+//Therefore need to code it here.
+
+//filter plugin_row_meta data to add a settings link to plugin information on "Manage Plugins" page.
+
+/***Please note the following links will only appear after activating the plugin.***/
+add_filter('plugin_row_meta', 'icopyright_settings_link', 10, 2);
+
+//function to create settings link
+function icopyright_settings_link($links, $file) {
+    if ($file == plugin_basename(__FILE__)) {
+        $settings_link = "<a href=\"options-general.php?page=icopyright\">Settings</a>";
+        $links[] = $settings_link;
+    }
+    return $links;
+}
+?>
---
+++ icopyright_xml.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Feed Template for displaying iCopyright feed, modified from WordPress core file wp-includes/feed-rss2.php
+ */
+//include wp-config
+$root = dirname(dirname(dirname(dirname(__FILE__))));
+if (file_exists($root.'/wp-load.php')) {
+// WP 2.6
+require_once($root.'/wp-load.php');
+} else {
+// Before 2.6
+require_once($root.'/wp-config.php');
+}
+//include $wpdb class
+require_once(ABSPATH . WPINC . '/wp-db.php');
+
+//function to format date
+function format_date($date)
+{
+    $full_date = explode(' ',$date);
+    $date =  explode('-',$full_date[0]);
+    $year = $date['0'];
+    $month = $date['1'];
+    $day = $date['2'];
+    return $month.'/'.$day.'/'.$year;
+}
+
+//get id http queried from icopyright conductor
+$icopyright_post_id = $_GET['id'];
+
+global $wpdb;
+
+$posttable = $wpdb->prefix."posts";
+
+$usertable = $wpdb->prefix."users";
+
+$response = $wpdb->get_results("SELECT * FROM $posttable JOIN $usertable on $posttable.post_author=$usertable.ID WHERE $posttable.ID = '$icopyright_post_id'");
+foreach ($response as $res){
+
+header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
+$more = 1;
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+
+
+    // prepare some variable for the XML feed
+    $icx_author = $res->display_name;
+    
+    
+	// get the User Role
+    $user = new WP_User($res->post_author);
+
+    if ( !empty( $user->roles ) && is_array( $user->roles ) ) {
+	foreach ( $user->roles as $role )
+		$icx_byline = $role;
+    }
+
+
+    // get copyrights
+    $publish_date = $res->post_date;
+    $date = explode('-',$publish_date);
+    $icx_copyright = $date['0']." ".get_bloginfo();
+    $icx_pubyear =  $date['0'];
+
+    // get publication date
+    $icx_pubdate = format_date($publish_date);
+
+    // get heading
+    $icx_headline = $res->post_title;
+
+    //get story
+    $icx_story = nl2br($res->post_content);
+
+    //get url
+    $icx_url = get_permalink($icopyright_post_id);
+
+    // get category
+    $category = get_the_category($icopyright_post_id);
+    $icx_section = $category[0]->cat_name;
+
+	// Construct the XML feed output
+	$xml = "<icx>\n";
+	$xml.="<icx_authors>$icx_author</icx_authors>\n";
+	$xml.="<icx_byline>$icx_byline</icx_byline>\n";
+	$xml.="<icx_copyright>$icx_copyright</icx_copyright>\n";
+	$xml.="<icx_deckheader>$icx_headline</icx_deckheader>\n";
+	$xml.="<icx_headline>$icx_headline</icx_headline>\n";
+	$xml.="<icx_pubdate>$icx_pubdate</icx_pubdate>\n";
+	$xml.="<icx_pubyear>$icx_pubyear</icx_pubyear>\n";
+	$xml.="<icx_section>$icx_section</icx_section>\n";
+	$xml.="<icx_story>$icx_story</icx_story>\n";
+	$xml.="<icx_url>$icx_url</icx_url>\n";
+	$xml.="</icx>";
+	
+	echo $xml;
+}
+?>
