1da222e3b5b5994855d5c7556d77173aaf33f75a
icopyright <icopyright@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-05 22:44:38 +0000

    Bump version number in useragent string
    git-svn-id: https://plugins.svn.wordpress.org/copyright-licensing-tools/trunk@471338 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- icopyright.php
+++ icopyright.php
@@ -28,7 +28,7 @@ define('ICOPYRIGHT_PRINTR_RESPONSE','false');
 include (ICOPYRIGHT_PLUGIN_DIR . '/icopyright-common.php');
 
 //define user agent
-define("ICOPYRIGHT_USERAGENT", "iCopyright WordPress Plugin v1.3");
+define("ICOPYRIGHT_USERAGENT", "iCopyright WordPress Plugin v1.4");
 
 //define URL to iCopyright; assuming other file structures will be intact.
 //url constructed from define server from icopyright-common.php
