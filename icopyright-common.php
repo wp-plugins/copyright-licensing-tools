a5cfff9b7e0f514a6b7fa5501432361356521f90
icopyright <icopyright@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-21 01:37:37 +0000

    Only set followlocation if it's possible to do so
    git-svn-id: https://plugins.svn.wordpress.org/copyright-licensing-tools/trunk@656043 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- icopyright-common.php
+++ icopyright-common.php
@@ -326,7 +326,10 @@ function icopyright_post($url, $postdata, $useragent = NULL, $headers = NULL) {
     curl_setopt($rs_ch, CURLOPT_POST, 1);
     curl_setopt($rs_ch, CURLOPT_POSTFIELDS, $postdata);
   }
-  curl_setopt($rs_ch, CURLOPT_FOLLOWLOCATION, 1);
+  // Very unlikely we will need to follow, but set if we can
+  if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) {
+    curl_setopt($rs_ch, CURLOPT_FOLLOWLOCATION, 1);
+  }
   curl_setopt($rs_ch, CURLOPT_HEADER, 0);
   if ($headers != NULL) {
     curl_setopt($rs_ch, CURLOPT_HTTPHEADER, $headers);
