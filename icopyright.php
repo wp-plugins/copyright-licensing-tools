a051eb8fc9b31ddf94dbd296a94d02ac297b25b4
icopyright <icopyright@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-28 09:32:19 +0000

    Changed version and settings link.
    git-svn-id: https://plugins.svn.wordpress.org/copyright-licensing-tools/trunk@688343 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- icopyright.php
+++ icopyright.php
@@ -5,7 +5,7 @@ Plugin URI: http://info.icopyright.com/wordpress
 Description: The iCopyright plugin adds article tools (print, email, post, and republish) and an interactive copyright notice to your site that facilitate the monetization and distribution of your content. Earn fees or ad revenue when your articles are re-used. Identify websites that re-use your content without permission and request takedown or convert them to customers. By iCopyright, Inc.
 Author: iCopyright, Inc.  
 Author URI: http://info.icopyright.com
-Version: 1.7.2
+Version: 1.8.0
 */
 
 //define constant that need to be changed from test environment to live environment
@@ -44,7 +44,7 @@ add_filter('plugin_row_meta', 'icopyright_settings_link', 10, 2);
 //function to create settings link
 function icopyright_settings_link($links, $file) {
   if ($file == plugin_basename(__FILE__)) {
-    $settings_link = "<a href=\"options-general.php?page=icopyright.php\">Settings</a>";
+    $settings_link = "<a href=\"options-general.php?page=copyright-licensing-tools\">Settings</a>";
     $video_link = "<a href=\"http://info.icopyright.com/icopyright-video\" target=\"_blank\">View a video introduction to iCopyright</a>"; //added version 1.1.2
     $links[] = $settings_link;
     $links[] .= $video_link; //added version 1.1.2
