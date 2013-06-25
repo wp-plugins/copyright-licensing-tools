e91a8a94de7fac928baf2deb8c6a50f0c4903a89
icopyright <icopyright@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-25 20:01:50 +0000

    Prefer term "address" to "account settings" for the address part of the form
    git-svn-id: https://plugins.svn.wordpress.org/copyright-licensing-tools/trunk@731678 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/main.js
+++ js/main.js
@@ -70,12 +70,12 @@ jQuery(document).ready(function () {
     jQuery("#toggle_account_setting").toggle(function () {
             jQuery(this).next().show();
             jQuery(this).next().next().show();
-            jQuery("#toggle_account_setting").val("Hide Account Settings");
+            jQuery("#toggle_account_setting").val("Hide Address");
         },
         function () {
             jQuery(this).next().hide();
             jQuery(this).next().next().hide();
-            jQuery("#toggle_account_setting").val("Show Account Settings")
+            jQuery("#toggle_account_setting").val("Show Address")
         }
     );
     jQuery("#toggle_account_setting").next().hide();
