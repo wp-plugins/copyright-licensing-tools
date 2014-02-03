18bd7f8d6adb20bab8b40e98d2af4fd06f9317aa
icopyright <icopyright@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-03 14:56:22 +0000

    Add WP video to settings page.
    git-svn-id: https://plugins.svn.wordpress.org/copyright-licensing-tools/trunk@850595 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- icopyright-admin.php
+++ icopyright-admin.php
@@ -37,6 +37,9 @@ function icopyright_options_page() {
   //
   wp_enqueue_style('icopyright-admin-css', plugins_url('css/style.css', __FILE__), array(), '1.0.0');  // Update the version when the style changes.  Refreshes cache.
   wp_enqueue_script('icopyright-admin-js', plugins_url('js/main.js', __FILE__));
+  wp_enqueue_style('icopyright-fancybox-css', plugins_url('css/jquery.fancybox.css', __FILE__));  // Update the version when the style changes.  Refreshes cache.
+  wp_enqueue_script('icopyright-fancybox-js', plugins_url('js/jquery.fancybox.js', __FILE__));
+  wp_enqueue_script('icopyright-fancybox-media-js', plugins_url('js/jquery.fancybox-media.js', __FILE__));
 
   $tou = get_option('icopyright_tou');
   if (($touResult != NULL && $touResult == 'FAILURE') || ($registrationResult != NULL && $registrationResult == 'FAILURE') || !empty($_GET['show-registration-form'])) {
@@ -111,13 +114,74 @@ function icopyright_options_page() {
 function account_settings_section_callback() {
   $address = get_option('icopyright_address_line1');
   if (!empty($address)) {
-    print '<input type="button" id="toggle_account_setting" value="Show Address" style="cursor:pointer; margin-top: 1em;">';
+?>
+    <input type="button" id="toggle_account_setting" value="Show Address" style="cursor:pointer; margin-top: 1em;">
+<?php
   } else {
-    print '<h3>Send Revenue Checks To:</h3>';
+?>
+    <h3>Send Revenue Checks To:</h3>
+    <div style="position:relative;float:right;width:500px;">
+      <a href="http://www.youtube.com/watch?v=bpYG-Frhh9E&autoplay=1" style="width:500px;position:absolute;" class="fancybox-youtube" id="icopyright_wp_settings_video" title="iCopyright WordPress Settings">
+        <img src="/wp-content/plugins/copyright-licensing-tools/images/bpYG-Frhh9E-mq.png" width="500px" style="border: 1px solid black"/>
+        <img src="/wp-content/plugins/copyright-licensing-tools/images/btn.play.png" style="position:absolute;left:190px;top:65px;opacity:.5;"/>
+      </a>
+    </div>
+    <script type="text/javascript">
+      jQuery(document).ready(function() {
+        jQuery('a.fancybox-youtube, area.fancybox-youtube').fancybox({
+          'padding'		: 0,
+          'autoScale'		: false,
+          'transitionIn'	: 'none',
+          'transitionOut'	: 'none',
+          'title'			: jQuery("#icopyright_wp_settings_video").attr("title"),
+          'width'		: 680,
+          'height'		: 495,
+          'href'			: jQuery("#icopyright_wp_settings_video").attr("href").replace(new RegExp("watch\\?v=", "i"), 'v/'),
+          'type'			: 'swf',
+          'swf'			: {
+            'wmode'		: 'transparent',
+            'allowfullscreen'	: 'true'
+          }
+        });
+      });
+    </script>
+    <div style="float:left; width: 700px;">
+<?php
   }
 }
 
 function deployment_mechanism_section_callback() {
+  $address = get_option('icopyright_address_line1');
+  if (!empty($address)) {
+  ?>
+<div style="position:relative;float:right;width:350px;">
+  <a href="http://www.youtube.com/watch?v=bpYG-Frhh9E&autoplay=1" style="width:350px;position:absolute;" class="fancybox-youtube" id="icopyright_wp_settings_video" title="iCopyright WordPress Settings">
+    <img src="/wp-content/plugins/copyright-licensing-tools/images/bpYG-Frhh9E-mq.png" width="350px" style="border: 1px solid black"/>
+    <img src="/wp-content/plugins/copyright-licensing-tools/images/btn.play.png" style="position:absolute;left:115px;top:25px;opacity:.5;"/>
+  </a>
+</div>
+<script type="text/javascript">
+  jQuery(document).ready(function() {
+    jQuery('a.fancybox-youtube, area.fancybox-youtube').fancybox({
+        'padding'		: 0,
+        'autoScale'		: false,
+        'transitionIn'	: 'none',
+        'transitionOut'	: 'none',
+        'title'			: jQuery("#icopyright_wp_settings_video").attr("title"),
+        'width'		: 680,
+        'height'		: 495,
+        'href'			: jQuery("#icopyright_wp_settings_video").attr("href").replace(new RegExp("watch\\?v=", "i"), 'v/'),
+        'type'			: 'swf',
+        'swf'			: {
+          'wmode'		: 'transparent',
+          'allowfullscreen'	: 'true'
+        }
+      });
+  });
+</script>
+<div style="float:left; width: 850px;">
+  <?php
+  }
   ?>
 <p>
   The following settings will determine how the iCopyright Toolbar and Interactive Copyright Notice appear on your
@@ -125,10 +189,22 @@ function deployment_mechanism_section_callback() {
     href="mailto:wordpress@icopyright.com">wordpress@icopyright.com</a> or get <a
     href="http://info.icopyright.com/wordpress" target="_blank">help</a>.
 </p>
-<?php
+  <?php
+  if (empty($address)) {
+  ?>
+</div>
+  <?php
+  }
 }
 
 function toolbar_appearance_section_callback() {
+  $address = get_option('icopyright_address_line1');
+  if (!empty($address)) {
+    ?>
+    </div>
+    <div style="clear: both;"></div>
+  <?php
+  }
 }
 
 function display_section_callback() {
